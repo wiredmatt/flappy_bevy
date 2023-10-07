7a3b6420c72713f0cbbf774bf7ec249a2f02bfb0
wiredmatt <29718978+wiredmatt@users.noreply.github.com>
2023-10-06 23:05:03 -0300

    chore: cleanup, vscode launch cfg, add assets

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,7 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, HelloPlugin))
+        .add_plugins(DefaultPlugins)
         .run();
-}
-
-fn greet_people(
-    time: Res<Time>,
-    mut timer: ResMut<GreetTimer>,
-    query: Query<&Name, With<Person>>,
-) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer
-        .0
-        .tick(time.delta())
-        .just_finished()
-    {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Mateo C".to_string())));
-    commands.spawn((Person, Name("Marcelo C".to_string())));
-    commands.spawn((Person, Name("Clara C".to_string())));
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(
-            Timer::from_seconds(2.0, TimerMode::Repeating),
-        ))
-        .add_systems(Startup, add_people)
-        .add_systems(Update, greet_people);
-    }
 }
